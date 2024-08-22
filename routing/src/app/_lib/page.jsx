e7407de8f12369ea68c0c505ad0b545788eecfc4
mannu-cyber <sahanimannu844@gmai.com>
2024-08-22 09:14:29 +0530

    private folders

---
+++ routing/src/app/_lib/page.jsx
@@ -0,0 +1,7 @@
+export default function PrivateRoute() {
+  return (
+    <div>
+      <h1>you can not see this file</h1>
+    </div>
+  );
+}
--- routing/src/app/not-found.jsx
+++ routing/src/app/not-found.jsx
@@ -1,13 +1,14 @@
 import Saman from "./saman/page";
 
-export default function NotFondPage(
-  name,
-  saman,
-  dusra,
-  teeshra,
-  chauta,
-  pachawa
-) {
+export default function NotFoundPage() {
+  return (
+    <div>
+      <h1>Page not found 404</h1>
+    </div>
+  );
+}
+
+function NotFondPage(name, saman, dusra, teeshra, chauta, pachawa) {
   return (
     <div>
       <Saman
